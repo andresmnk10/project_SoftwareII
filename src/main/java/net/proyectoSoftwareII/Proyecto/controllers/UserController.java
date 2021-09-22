6ca87a4669d7b01b02237698c3615192259cc980
andresmnk10 <pipenk10ma@gmail.com>
2021-09-22 17:00:14 -0500

    Se agregó utilidades añ UserController

--- src/main/java/net/proyectoSoftwareII/Proyecto/controllers/UserController.java
+++ src/main/java/net/proyectoSoftwareII/Proyecto/controllers/UserController.java
@@ -1,12 +1,15 @@
 package net.proyectoSoftwareII.Proyecto.controllers;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import net.proyectoSoftwareII.Proyecto.models.reponses.UsersRest;
 import net.proyectoSoftwareII.Proyecto.models.request.UserDetailRequestModel;
+import net.proyectoSoftwareII.Proyecto.shared.dto.UserDTO;
 
 @RestController
 @RequestMapping ("/users")
@@ -19,6 +22,18 @@ public class UserController {
 
     @PostMapping
     public UsersRest createUser (@RequestBody UserDetailRequestModel userDetails){
+
+        UserRest userToReturn = new UserRest();
+        
+        UserDTO userDTO = new UserDTO ();
+
+        BeanUtils.copyProperties(userDetails, userDTO);
+
+        UserDTO createdUser = userService.createdUser(userDTO);
+        
+        BeanUtils.copyProperties(createdUser, userToReturn);
+         
+
         return null;
     } 
 
